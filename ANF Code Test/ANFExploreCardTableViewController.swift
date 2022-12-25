2ff64924ebe46f475b820c2c6d863fe15cf05ce4
vamshi <j.vamshiraj11@gmail.com>
2022-12-25 15:13:23 -0600

    added String extension for adding html attributes

--- ANF Code Test/ANFExploreCardTableViewController.swift
+++ ANF Code Test/ANFExploreCardTableViewController.swift
@@ -45,3 +45,18 @@ class ANFExploreCardTableViewController: UITableViewController {
         }
     }
 }
+
+// MARK: - extenstion
+extension String {
+    var htmlToAttributedString: NSAttributedString? {
+        guard let data = data(using: .utf8) else { return nil }
+        do {
+            return try NSAttributedString(data: data, options: [.documentType: NSAttributedString.DocumentType.html, .characterEncoding:String.Encoding.utf8.rawValue], documentAttributes: nil)
+        } catch {
+            return nil
+        }
+    }
+    var htmlToString: String {
+        return htmlToAttributedString?.string ?? ""
+    }
+}
