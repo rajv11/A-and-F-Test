04c92eaad9f6b810b2da44cb4da3c19020249eac
vamshi <j.vamshiraj11@gmail.com>
2022-12-26 16:37:38 -0600

    refactored backgroundImage to backgroundImageView

--- ANF Code Test/PromoCardViewController.swift
+++ ANF Code Test/PromoCardViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class PromoCardViewController: UIViewController {
     @IBOutlet weak var topDescription: UILabel!
-    @IBOutlet weak var backgroundImage: UIImageView!
+    @IBOutlet weak var backgroundImageView: UIImageView!
     @IBOutlet weak var promoTitle: UILabel!
     @IBOutlet weak var promoMessage: UILabel!
     @IBOutlet weak var bottomDescription: UITextView!
@@ -39,12 +39,12 @@ class PromoCardViewController: UIViewController {
         var aspectRatio: CGFloat = 0.0
         let isPotrait = image.size.width < image.size.height
         aspectRatio = isPotrait ? image.size.height/image.size.width: image.size.width/image.size.height
-        backgroundImage.translatesAutoresizingMaskIntoConstraints = false
-        backgroundImage.image = image
-        backgroundImage.contentMode = .scaleAspectFit
+        backgroundImageView.translatesAutoresizingMaskIntoConstraints = false
+        backgroundImageView.image = image
+        backgroundImageView.contentMode = .scaleAspectFit
         
         // Adding height constraint to backgroundImageView
-        let constraint = isPotrait ? backgroundImage.widthAnchor.constraint(equalTo: backgroundImage.heightAnchor, multiplier: 1/aspectRatio) : backgroundImage.heightAnchor.constraint(equalTo: backgroundImage.widthAnchor, multiplier: 1/aspectRatio)
+        let constraint = isPotrait ? backgroundImageView.widthAnchor.constraint(equalTo: backgroundImageView.heightAnchor, multiplier: 1/aspectRatio) : backgroundImageView.heightAnchor.constraint(equalTo: backgroundImageView.widthAnchor, multiplier: 1/aspectRatio)
         NSLayoutConstraint.activate([constraint])
         
         promoMessage.textColor = .gray
