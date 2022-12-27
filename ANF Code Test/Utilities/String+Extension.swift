e1294685ef949c20d307d1cf65a00a4275bd70ce
vamshi <j.vamshiraj11@gmail.com>
2022-12-27 00:30:02 -0600

    added utilities folder and created StringExtension file

---
+++ ANF Code Test/Utilities/String+Extension.swift
@@ -0,0 +1,22 @@
+//
+//  String+Extension.swift
+//  ANF Code Test
+//
+//  Created by raj on 12/27/22.
+//
+
+import Foundation
+// MARK: - extenstion
+extension String {
+    var htmlToAttributedString: NSAttributedString? {
+        guard let data = data(using: .utf8) else { return nil }
+        do {
+            return try NSAttributedString(data: data, options: [.documentType: NSAttributedString.DocumentType.html, .characterEncoding:String.Encoding.utf8.rawValue], documentAttributes: nil)
+        } catch {
+            return nil
+        }
+    }
+    var htmlToString: String {
+        return htmlToAttributedString?.string ?? ""
+    }
+}
