b63600a0c5b152dd7aa917b2949b2163f4e85bbe
vamshi <j.vamshiraj11@gmail.com>
2022-12-27 13:06:06 -0600

    Created a JSON setup utility

---
+++ ANF Code Test/Utilities/Utilities.swift
@@ -0,0 +1,20 @@
+//
+//  Utilities.swift
+//  ANF Code Test
+//
+//  Created by raj on 12/27/22.
+//
+
+import Foundation
+
+class Utilities {
+    class func setUpJsonFile(jsonFileName: String?) -> Data? {
+        guard let path = Bundle.main.path(forResource: jsonFileName, ofType: "json") else { return nil }
+        do {
+            return try Data(contentsOf: URL(fileURLWithPath: path), options: .alwaysMapped) as Data
+        } catch let error {
+            debugPrint(error.localizedDescription)
+        }
+        return nil
+    }
+}
